22c0032259e0b2cc710587e1403d41fabdc0df9a
quockhanh1702 <74188095+quockhanh1702@users.noreply.github.com>
2022-03-17 22:38:14 +0700

    dasdasdasd

--- flutter_application_book/lib/models/book.dart
+++ flutter_application_book/lib/models/book.dart
@@ -1,13 +1,14 @@
+import 'dart:convert';
 class Book {
-  Book(
-      {required this.id,
-      required this.title,
-      required this.description,
-      required this.date,
-      required this.author,
-      required this.image,
-      required this.nameCategory,
-      required this.idCategory});
+  Book({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.date,
+    required this.author,
+    required this.image,
+    required this.categoryName,
+  });
 
   int id;
   String title;
@@ -15,18 +16,46 @@ class Book {
   DateTime date;
   String author;
   String image;
-  String nameCategory;
-  int idCategory;
+  String categoryName;
 
   factory Book.fromJson(Map<String, dynamic> json) {
     return Book(
-        id: json["id"],
-        title: json["title"],
-        description: json["description"],
-        date: json["date"],
-        author: json["author"],
-        image: json["image"],
-        nameCategory: json["nameCategory"],
-        idCategory: json["idCategory"]);
+      id: json["id"],
+      title: json["title"],
+      description: json["description"],
+      date: json["date"],
+      author: json["author"],
+      image: json["image"],
+      categoryName: json["categoryName"],
+    );
   }
+
+  Map<String, dynamic> toJson() {
+    return {
+      "id": id,
+      "title": title,
+      "description": description,
+      "categoryName": categoryName,
+      "date": date,
+      "image": image,
+      "author": author,
+    };
+  }
+
+   @override
+  String toString() {
+    return 'Book{id: $id, title: $title, description: $description, date: $date, author: $author, image: $image, categoryName: $categoryName}';
+  }
+}
+
+List<Book> bookFromJson(String strJson) {
+  final str = json.decode(strJson);
+  return List<Book>.from(str.map((item) {
+    return Book.fromJson(item);
+  }));
+}
+
+String bookToJson(Book data) {
+  final dyn = data.toJson();
+  return json.encode(dyn);
 }
